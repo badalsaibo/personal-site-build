d68dff31eeb9fde1e6736328536d8f6b2b4cf5d2
heyDante <badalsaibo@gmail.com>
2020-02-28 10:32:06 +0530

    uibuild

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2eab951263661c1f1253234835b80c83.js"
+  "/precache-manifest.98535a2fea1badb0dc595b3bd0b4b58b.js"
 );
 
 self.addEventListener('message', (event) => {
